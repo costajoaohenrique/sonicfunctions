53f82c79f8eb5068d8c6ce888c4d48c5fcabb8ba
Joao Henrique Da Silva Costa <costa.joaohenrique@gmail.com>
2019-05-29 19:32:36 -0300

    Configuração Inicial para JNSql

---
+++ register-thorntail/src/main/java/com/sonicfunctions/register/thorntail/domain/Function.java
@@ -0,0 +1,57 @@
+package com.sonicfunctions.register.thorntail.domain;
+
+import org.jnosql.artemis.Column;
+import org.jnosql.artemis.Entity;
+import org.jnosql.artemis.Id;
+
+/**
+ * Function
+ */
+@Entity
+public class Function {
+
+    @Id
+    private long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String body;
+
+    @Column
+    private String type;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+}
